6c0840987c36af70851970bb0c0797655568b389
grantmukai <grantmukai@users.noreply.github.com>
2015-10-17 00:03:16 -0400

    Update lang.en.us.php

--- languages/lang.en.us.php
+++ languages/lang.en.us.php
@@ -8,5 +8,32 @@ Language: English - US
 $lang = array();
  
 $lang['PAGE_TITLE'] = 'Grant Mukai';
+$lang['NAME'] = 'Grant Kiyoshi Mukai';
+
+// Menu bar
+
+$lang['intro'] = 'home';
+$lang['profile'] = 'profile';
+$lang['services'] = 'services';
+$lang['resume'] = 'Curriculum Vitae';
+$lang['portfolio'] = 'My Work';
+$lang['contact'] = 'Contact Me';
+
+$lang['hello_1'] = "Hello, I'm Grant Kiyoshi Mukai";
+$lang['hello_2'] = 'Front-end Developer. | Database Manager. | Email Marketer. | Video Editor.';
+$lang['hello_3'] = 'Front-end Developer. Database Manager. Email Marketer.';
+
+$lang['bio'] = "
+<h3>Oh hell yeah!</h3>
+<p>It's gonna be sooooo much fun</p>
+<p>filling this shit out!</p>
+";
+
+$lang['status_title'] = 'My status';
+$lang['status_1'] = "I currently work for the Political Science Association";
+$lang['status_2'] = "I work in the Communications &amp; Marketing Department helping our members better connect to the field of political science.";
+$lang['status_3'] = 'Contact me';
+
+$lang['customers'] = 'Companies I have worked for';
 
 ?>
