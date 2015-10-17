73a0c3ac10bc5b8e0a3050cf0ccac1c3ba1b2f53
grantmukai <grantmukai@users.noreply.github.com>
2015-10-17 19:37:58 -0400

    English Canada lang php added

---
+++ languages/lang.en.ca.php
@@ -0,0 +1,60 @@
+<?php
+/*
+----------------------
+Language: English - Canada
+----------------------
+*/
+ 
+$lang = array();
+ 
+$lang['PAGE_TITLE'] = 'Grant Mukai';
+$lang['NAME'] = 'Grant Kiyoshi Mukai';
+
+// Menu bar
+
+$lang['intro'] = 'home';
+$lang['profile'] = 'profile';
+$lang['services'] = 'services';
+$lang['resume'] = 'Curriculum Vitae';
+$lang['portfolio'] = 'My Work';
+$lang['contact'] = 'Contact Me';
+
+$lang['hello_1'] = "Hello, I'm Grant Kiyoshi Mukai";
+$lang['hello_2'] = 'Front-end Developer. | Database Manager. | Email Marketer. | Video Editor.';
+$lang['hello_3'] = 'Front-end Developer. Database Manager. Email Marketer.';
+
+$lang['skillset'] = 'Skillset';
+
+$lang['social_profiles'] = 'Social Profiles';
+
+$lang['bio'] = "
+<h3>Who Am I?</h3>
+
+<p>My name is Grant Kiyoshi Mukai and I am a Web developer and systems support specialist for the <a href='http://www.apsanet.org'>American Political Science Association</a> (APSA) in Washington, District of Columbia, United States of America. I work on a small team in the Marketing and Communications Department where I help manage our association's public-facing site as well as several small-scale WordPress sites devoted to promoting political science and helping our special interest sections organize themselves on the Web. I also help out with a lot of database management, statistical analysis, and report generation while providing excellent customer service to our members in English, French, and Spanish. Although we are located in the United States, we have a very large international membership.</p>
+
+<p>Before joining the APSA, I worked at both the <a href='http://msb.georgetown.edu'>Georgetown University McDonough School of Business</a> in Washington, D.C. and <a href='http://www.iqsolutions.com'>iQ Solutions</a> in Rockville, Maryland developing email newsletters. Some of the healthcare clients I worked for include the Substance Abuse and Mental Health Services Administration (SAMHSA) and the National Institutes of Health (NIH). I have also produced and updated English and Spanish webpages through coding HTML, CSS, and PHP for the National Institutes of Arthritis and Musculoskeletal and Skin Diseases (NIAMS).</p>
+
+<p>A large part of my responsibilities have comprised making sure that webpages and documents meet the Web Accessibility Guidelines set forth by the World Wide Web Consortium and Section 508 of the Americans with Disabilities Act.</p>
+
+<p>Prior to my career switch from journalism to Web development and database management, I was an Associate News Producer for the <a href='http://www.wellesleymedia.org'>Wellesley Media Corporation</a> in the town of Wellesley, Massachusetts where he produced news reports and documentaries for the local cable access station. In the Boston area, I was instrumental in bringing online video content to the Patch.com platform where I provided freelance video work on a number of local events and businesses and ran a weekly real estate column.</p>
+
+<p>While at university, I gained additional television experiences from the <a href='http://www.wgbh.org'>WGBH Educational Foundation (PBS)</a> as the Antiques Roadshow Web Intern and with <a href='http://www.publicsynergies.com'>Public Synergies</a> (in French) and Rpublictv in Asnières-sur-Seine, France, as a multimedia and video intern.</p>
+
+<p>I am a proud alumnus of Boston University's <a href='http://www.bu.edu/com'>College of Communication</a> where I graduated with a Bachelor of Science (first cycle degree) in Broadcast Journalism and a minor (specialty) in French Language and Literature.</p>
+";
+
+$lang['my_stats'] = 'My Stats';
+$lang['cups_of_coffee'] = 'Cups of coffee';
+$lang['lines_of_code'] = 'Lines of code';
+$lang['happy_customers'] = 'Happy customers';
+
+$lang['status_title'] = 'My status';
+$lang['status_1'] = "I currently work for the American Political Science Association";
+$lang['status_2'] = "I work in the Communications &amp; Marketing Department helping our members better connect to the field of political science.";
+$lang['status_3'] = 'Contact me';
+
+$lang['customers'] = 'Companies I have worked for';
+
+$lang['copyright'] = 'Grant Mukai. All rights reserved. Canada.';
+
+?>
