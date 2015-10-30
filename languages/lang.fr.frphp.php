af55908ad324923747d7e3c7f475df38142b67e1
El Capitan Macbook Air <grantmukai@icloud.com>
2015-10-29 20:08:32 -0400

    La FRANCE ajouté

---
+++ languages/lang.fr.frphp.php
@@ -0,0 +1,75 @@
+<?php date_default_timezone_set("Europe/Paris"); ?>
+<?php
+/*
+-------------------------
+Langue: Français - France
+-------------------------
+*/
+$cv_lang = "fr-FR";
+ 
+$lang = array();
+ 
+$lang['PAGE_TITLE'] = 'Grant Muka&iuml;';
+$lang['NAME'] = 'Grant Kiyoshi Muka&iuml;';
+
+// Menu bar
+
+$lang['intro'] = 'Acceuil';
+$lang['profile'] = 'Profil';
+$lang['services'] = 'services';
+$lang['resume'] = 'Curriculum Vitae';
+$lang['portfolio'] = 'Mon travail';
+$lang['contact'] = 'Contactez-Moi';
+
+$lang['hello_1'] = "Bonjour, je m'appelle Grant Kiyoshi Muka&iuml;";
+$lang['hello_2'] = 'D&eacute;veloppeur Web. | G&eacute;rant des bases de donn&eacute;es. | Agent de Marketing. | Monteur Vid&eacute;o.';
+$lang['hello_3'] = 'D&eacute;veloppeur Web. G&eacute;rant des Bases de Donn&eacute;es. Agent de Marketing.';
+
+$lang['select_country'] = "Choisissez votre pays";
+
+$lang['skillset'] = 'Gamme de comp&eacute;tences';
+
+$lang['social_profiles'] = 'R&eacute;seaux Sociaux';
+
+$lang['bio'] = "
+<h3>Qui Je Suis</h3>
+
+<p>Je m'appelle Grant Kiyoshi Muka&iuml; et je suis D&eacute;veloppeur Web et Sp&eacute;cialiste en services et syst&egrave;mes &agrave; <a href='http://www.apsanet.org/'>l'Association am&eacute;ricaine de science politique</a> (APSA) &agrave; Washington, &Eacute;tats-Unis d'Am&eacute;rique. Je travaille sur une petite &eacute;quipe dans le D&eacute;partement de promotion et communications o&ugrave; j'aide mes co&eacute;quipiers &agrave; g&eacute;rer le site publique de l'association ainsi que plusieurs sites WordPress consacr&eacute;s &agrave; la promotion de la science politique et qui aident nos sections d'int&eacute;r&ecirc;ts sp&eacute;calis&eacute;s &agrave; s'organiser aux r&eacute;seaux. Je suis aussi responsable de la gestion des bases de donn&eacute;es, de l'analyse statistique, et la g&eacute;n&eacute;ration des tableaux tout en fournissant de l'excellent service &agrave; la client&egrave;le en anglais, fran&ccedil;ais, et espagnol. Bien que nous nous situions aux &Eacute;tats-Unis, nous avons des membres venant de partout dans le monde.</p>
+
+<p>Avant de me joindre &agrave; l'APSA, j'ai travaill&eacute; &agrave; <a href='http://msb.georgetown.edu'>l'&Eacute;cole McDonough de commerce</a> &agrave; l'Universit&eacute; Georgetown &agrave; Washington et <a href='http://www.iqsolutions.com'>iQ Solutions</a> &agrave; dans les banlieux en d&eacute;veloppant des bulletins &eacute;lectroniques. Quelques clients de sant&eacute; pour lesquels j'ai travaill&eacute; ont compris l'Administration pour abus des stup&eacute;fiants et services sant&eacute; mentale <em>(SAMHSA)</em> et les Instituts nationaux de la sant&eacute; <em>(NIH)</em>. J'ai aussi r&eacute;alis&eacute; et mit &agrave; jour des pages Web en anglais et en espagnol pour les Instituts nationaux de l'arthrite, affections musculo-squelettiques et maladies de la peau <em>(NIAMS)</em>.</p>
+
+<p>Une grande partie de mes responsabilit&eacute;s a inclut la v&eacute;rification des pages Web et documents PDF afin d'assurer la conformit&eacute; aux normes &eacute;tablies par le W3C <em>(World Wide Web Consortium)</em>.</p>
+
+<p>Avant mon changement de carri&egrave;re du journalisme au d&eacute;veloppement Web et gestion des donn&eacute;es, j'&eacute;tais r&eacute;alisateur adjoint d'actualit&eacute; chez <a href='http://www.wellesleymedia.org'>Wellesley Media Corporation</a> dans le village de Wellesley, Massachusetts, o&ugrave; je produisais des court m&eacute;trages et documentaires pour une cha&icirc;ne de t&eacute;l&eacute;vision. Dans la m&eacute;tropole de Boston, j'ai apport&eacute; du contenu vid&eacute;o au plateforme Patch.com.</p>
+
+<p>&Agrave; l'universit&eacute;, j'ai gagn&eacute; encore plus d'exp&eacute;rience en t&eacute;l&eacute;vision lors de mon stage en tant que stagiaire Web pour Antiques Roadshow au WGBH Educational Foundation (PBS) chez <a href='http://www.publicsynergies.com'>Public Synergies</a> et Rpublictv &agrave; Asni&egrave;res-sur-Seine, France, en tant que stagiaire vid&eacute;o et multim&eacute;dia.</p>
+
+<p>J'ai obtenu un BAC+4 (dipl&ocirc;me de premier cycle) en journalisme t&eacute;l&eacute;vis&eacute; &agrave; <a href='http://www.bu.edu/com'>l'&Eacute;cole de communications</a> &agrave; l'Universit&eacute; de Boston avec une sp&eacute;cialisation en langue et litt&eacute;rature fran&ccedil;aise.</p>
+";
+
+$lang['my_stats'] = 'Mes Statistiques';
+
+$lang['cups_of_coffee'] = 'Tasses de caf&eacute;';
+$lang['lines_of_code'] = 'Lignes de code';
+$lang['happy_customers'] = 'Gens sur mon &eacute;quipe';
+
+$lang['web_development'] = "D&eacute;veloppement Web";
+$lang['public_relations'] = "Relations Publiques";
+$lang['experience'] = "Exp&eacute;riences Professionnelles";
+$lang['duties'] = "T&acirc;ches";
+$lang['education'] = "Formation";
+
+$lang['download_cv'] = "T&eacute;l&eacute;charger CV";
+$fileDate = new DateTime('10/27/2015');
+$lang['file_size'] = 3;
+
+$lang['status_title'] = 'Mon status';
+$lang['status_1'] = "Je travaille actuellement pour l'Association am&eacute;ricaine de science politique";
+$lang['status_2'] = "Je travaille dans le D&eacute;partement des communications &amp; marketing avec une &eacute;quipe formidable.";
+$lang['status_3'] = 'Contactez-moi';
+
+$lang['customers'] = "Soci&eacute;t&eacute;s pour lesquelles j'ai travaill&eacute";
+
+$lang['copyright'] = 'Grant Muka&iuml;. Tous droits r&eacute;serv&eacute;s.';
+
+?>
