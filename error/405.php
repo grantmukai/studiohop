fb61265f8dfb9e17c669866d79325b7a1e55eafc
El Capitan Macbook Air <grantmukai@icloud.com>
2015-11-10 19:38:11 -0500

    pages d'erreurs et htaccess en place

---
+++ error/405.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->  
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->  
+<head>
+    <title>Grant Mukai - 403</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Grant Mukai is a Web Developer and Systems Administrator in Washington, D.C., USA.">
+    <meta name="author" content="Grant Mukai">    
+    <link rel="shortcut icon" href="../favicon.ico">  
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<!-- CSS -->
+	<!-- Bootstrap core CSS -->
+	<link href="../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+	<!-- Owl Carousel -->
+	<link href="../assets/css/owl.theme.css" rel="stylesheet">
+	<link href="../assets/css/owl.carousel.css" rel="stylesheet">
+
+	<!-- Magnific-popup lightbox -->
+	<link href="../assets/css/magnific-popup.css" rel="stylesheet">
+
+	<!-- Simple text rotator -->
+	<link href="../assets/css/simpletextrotator.css" rel="stylesheet">
+
+	<!-- Font Awesome CSS -->
+	<link href="../assets/css/font-awesome.min.css" rel="stylesheet" media="screen">
+
+	<!-- Animate css -->
+	<link href="../assets/css/animate.css" rel="stylesheet">
+
+	<!-- Custom styles CSS -->
+	<link href="../assets/css/style.css" rel="stylesheet" media="screen">
+</head> 
+
+<body data-spy="scroll">
+    
+<!-- START error content-->
+<section class="license section">
+  <div class="container">
+    <div class="license-inner">
+      <div style="text-align:center;">
+		  <h1 style="font-size:48pt;">GrantMukai.com</h1>
+      <h1 style="font-size:48pt;">405</h1>
+
+      <div lang="en-US">
+      <h2>Method Not Allowed</h2>
+
+      <p>I'm afraid you aren't allowed to do that. <a href="http://www.grantmukai.com/index.php">Take me back.</a></p>
+      </div>
+
+      <div lang="fr-CA">
+      <h2>M&eacute;thode Interdite</h2>
+
+      <p>Malheureusement, vous n'avez pas le droit de faire &ccedil;a. <a href="http://www.grantmukai.com/fr/index.php">Rebobinez.</a></p>
+      </div>
+
+      <div lang="es-MX">
+      <h2>M&eacute;todo no permitido</h2>
+
+      <p>Lo siento. No tiene el derecho para est&aacute; acci&oacute;n. <a href="http://www.grantmukai.com/es/index.php">Revolver a casa.</a></p>
+      </div>
+    </div>
+  </div>
+</section>   
+<!-- END error content -->          
+</body>
+</html> 
+
