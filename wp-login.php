01aafb953931f150773e160a46e4ff354617203a
El Capitan Macbook Air <grantmukai@icloud.com>
2015-11-15 12:15:46 -0500

    wp login page

---
+++ wp-login.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>grantmukai.com &rsaquo; Log In</title>
+	<link rel='stylesheet' id='buttons-css'  href='http://www.grantmukai.com/wp-includes/css/buttons.min.css?ver=e4a8b550432264283a34331f0e2b0c83' type='text/css' media='all' />
+<link rel='stylesheet' id='open-sans-css'  href='https://fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&#038;subset=latin%2Clatin-ext&#038;ver=e4a8b550432264283a34331f0e2b0c83' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='http://www.grantmukai.com/wp-includes/css/dashicons.min.css?ver=e4a8b550432264283a34331f0e2b0c83' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='http://www.grantmukai.com/wp-admin/css/login.min.css?ver=e4a8b550432264283a34331f0e2b0c83' type='text/css' media='all' />
+<meta name='robots' content='noindex,follow' />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+	<div id="login">
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">grantmukai.com</a></h1>
+	
+<form name="loginform" id="loginform" action="http://www.grantmukai.com/process.php" method="post">
+	<p>
+		<label for="user_login">Username<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="hidden" name="redirect_to" value="http://www.grantmukai.com/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>	
+
+<p id="nav">
+	<a href="http://www.grantmukai.com/wp-login.php?action=lostpassword" title="Password Lost and Found">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="http://www.grantmukai.com/" title="Are you lost?">&larr; Back to grantmukai.com</a></p>
+	
+	</div>
+
+	
+	<link rel='stylesheet' id='jetpack_css-css'  href='http://www.grantmukai.com/wp-content/plugins/jetpack/css/jetpack.css?ver=e4a8b550432264283a34331f0e2b0c83' type='text/css' media='all' />
+	<div class="clear"></div>
+	</body>
+	</html>
+	
