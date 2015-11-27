1726a39204453a777ce832180af096f9cbc8475e
El Capitan Macbook Air <grantmukai@icloud.com>
2015-11-27 11:29:05 -0500

    màj ca web cv en

--- languages/lang.en.ca.php
+++ languages/lang.en.ca.php
@@ -66,8 +66,8 @@ $lang['duties'] = "Duties";
 $lang['education'] = "Education";
 
 $lang['download_webcv'] = "Download CV";
-$webFileDate = new DateTime('10/12/2015');
-$lang['webfile_size'] = 89;
+$webFileDate = new DateTime('11/25/2015');
+$lang['webfile_size'] = 68;
 
 $lang['download_prcv'] = "Download CV";
 $prFileDate = new DateTime('10/15/2015');
