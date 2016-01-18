66c2e30bdd840c731057e7677514e86cfbb85588
El Capitan Macbook Air <grantmukai@icloud.com>
2016-01-18 15:45:31 -0500

    Fotos mexicanas ahora funcionan

--- assets/images/villes/es-MX/credits.php
+++ assets/images/villes/es-MX/credits.php
@@ -1,13 +1,7 @@
 <?php
-	switch ($quelleVille) {
-	case 0:
-		$creditsImages = array("Ciudad de M&eacute;xico","M&eacute;xico, D.F.","Eneas De Troya","eneas");
-		break;
-	case 1:
-		$creditsImages = array("Centro de Guadalajara","Guadalajara, JAL","Arturo Avila","arturoav");
-		break;
-	case 2:
-		$creditsImages = array("Parroqu&iacute;a Nuestra Se&ntilde;ora del Refugi&oacute;n","Monterrey, NL","Enrique L&oacute;pez-Taayo Biosca","eltb");
-		break;
-}
+$creditsImages = array(
+	array(0,"Ciudad de M&eacute;xico","M&eacute;xico, D.F.","Eneas De Troya","eneas"),
+	array(1,"Centro de Guadalajara","Guadalajara, JAL","Arturo Avila","arturoav"),
+	array(2,"Parroqu&iacute;a Nuestra Se&ntilde;ora del Refugi&oacute;n","Monterrey, NL","Enrique L&oacute;pez-Taayo Biosca","eltb")
+);
 ?>
