f603e957ed5227a0981f7c3cd3d8e2f388d60484
Win10 PowerShell <grantmukai@icloud.com>
2016-01-23 15:55:25 -0500

    div transparent qui entoure les entetes

--- index.php
+++ index.php
@@ -119,7 +119,7 @@
 
 					<div class="col-md-12">
 
-						<div class="hello wow bounceInDown" style="color(160,160,160,.5);">
+						<div class="hello wow bounceInDown" style="padding:30px; background-color: rgba(5,4,2,0.4);">
 							<h1 class="textstroke"><?php echo $lang['hello_1']; ?></h1>
 							<h3 class="textstroke"><span class="rotate"><?php echo $lang['hello_2']; ?></span></h3>
 						</div>
