41b28d0d6aa87009097a61c4f2d5e55009c163b1
El Capitan Macbook Air <grantmukai@icloud.com>
2016-03-01 19:51:41 -0500

    fixed hailey and the bees order

--- portfolio/en.php
+++ portfolio/en.php
@@ -46,7 +46,8 @@ As The Price of Textbooks Weighs Heavily On Students Wallets, Some Administratio
 				<ul data-theme="default">
 			<?php
 				$wellesleyPlaylist = array(
-					"Hailey and the Bees, Part 1 (225MB)"=>"bees2.mov",
+					"Hailey and the Bees, Part 1 (147MB)"=>"bees1.mov",
+                    "Hailey and the Bees, Part 2 (225MB)"=>"bees2.mov",
 					"Arms Around Sierra Leone (84MB)"=>"sierraleone.mov",
 					"Radcliffe Bailey: Memory as Medicine (86MB)"=>"radcliffebailey.mov",
 					"Landscape Photographer Art Donahue (129MB)"=>"artdonahue.mov",
