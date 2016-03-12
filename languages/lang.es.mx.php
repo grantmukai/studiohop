791618ed03b58e076abfa5d8110f74b4e8b006c0
iMac El Capitan <grantmukai@icloud.com>
2016-03-11 20:52:49 -0500

    Subtítulos Mexicanos

--- languages/lang.es.mx.php
+++ languages/lang.es.mx.php
@@ -32,8 +32,8 @@ $lang['portfolio'] = 'Mi trabajo';
 $lang['contact'] = 'Contactarme';
 
 $lang['hello_1'] = "Hola, me llamo Grant Kiyoshi Mukai";
-$lang['hello_2'] = 'Desarrollador Web. | Gerente de bases de datos. | Agente de comunicaciones. | Editor de v&iacute;deo.';
-$lang['hello_3'] = 'Desarrollador Web. Gerente de bases de datos. Agente de comunicaciones.';
+$lang['hello_2'] = 'Desarrollador Web | Publicidad Correo Electr&oacute;nico | Administraci&oacute;n de Datos | Publicidad | Relaciones P&uacute;blicas | Periodismo | Gesti&oacute;n de contenidos Web | Comunicaciones';
+$lang['hello_3'] = 'Desarrollador Web | Publicidad Correo Electr&oacute;nico | Administraci&oacute;n de Datos | Publicidad';
 
 $lang['select_country'] = "Elige su pa&iacute;s";
 
