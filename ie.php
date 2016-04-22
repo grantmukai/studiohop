183794793f81c6edb258b93dfdf9ef152897afc3
Kiyoshi Mukai <hello@kiyoshimukai.com>
2016-04-22 16:16:13 -0400

    IE error page

---
+++ ie.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <title>Internet Explorer :(</title>
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Grant Mukai is a Web Developer and Systems Administrator in Washington, D.C., USA.">
+    <meta name="author" content="Grant Mukai">    
+    <link rel="shortcut icon" href="../favicon.ico">  
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,300italic,400italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	<!-- CSS -->
+	<!-- Bootstrap core CSS -->
+	<link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+	<!-- Custom styles CSS -->
+	<link href="assets/css/style.css" rel="stylesheet" media="screen">
+</head> 
+
+<body data-spy="scroll">
+    
+<!-- START error content-->
+<section class="license section">
+  <div class="container">
+    <div class="license-inner">
+      <div style="text-align:center;">
+		  <h1 style="font-size:48pt;">Sorry! D&eacute;sol&eacute;! &iexcl;Lo Siento!</h1>
+<br />
+      <div lang="en-US">
+      <h2>Internet Explorer is not supported</h2>
+
+      <p>Please try again with a different browser.</p>
+      </div>
+		<br />
+      <div lang="fr-CA">
+      <h2>Internet Explorer n'est pas support&eacute;</h2>
+
+      <p>SVP, r&ecacute;essayez avec un autre navigateur.</p>
+      </div>
+		<br />
+      <div lang="es-MX">
+      <h2>Internet Explorer no est&aacute; apoyado</h2>
+      <p>Reintenta con otro navegador.</p>
+      </div>
+    </div>
+  </div>
+</section>   
+<!-- END error content -->          
+</body>
+</html> 
+
