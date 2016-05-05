5282d5594e9016fb3497c61c6cfbdf3d496efe8d
Kiyoshi Mukai <hello@kiyoshimukai.com>
2016-05-05 19:25:01 -0400

    photos suisse

---
+++ assets/images/villes/fr-CH/credits.php
@@ -0,0 +1,11 @@
+<?php
+$creditsImages = array(
+	array(0,"BERNE 2015","Berne, BE","Isabelle S","101653318@N07"),
+	array(1,"Fribourg Gare","Fribourg, FR","Tobrouk","tobrouk"),
+    array(2,"Lausanne II","Lausanne, VD","Ville Miettinen","wili"),
+    array(3,"Sion, Valais, Switzerland","Sion, VS","G&eacute;rard M&eacute;trailler","gmetrail"),
+    array(4,"Neuch&acirc;tel, Switzerland","Neuch&acirc;tel, NE","Patrick Nouhailler","patrick_nouhailler"),
+    array(5,"Geneva","Gen&egrave;ve, GE","ITU Pictures","itupictures"),
+    array(6,"07.07.93 Del&eacute;mont SSB Re 4/4<sup>1</sup> 10050","Del&eacute;mont, JU","Phil Richards","philstephenrichards")
+    );
+?>
