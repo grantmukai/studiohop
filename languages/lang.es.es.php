e57156e573ee1101dadfc439d16114d8f55c7092
iMac El Capitan <grantmukai@icloud.com>
2016-05-09 23:45:50 -0400

    inicio pagina espanola

---
+++ languages/lang.es.es.php
@@ -0,0 +1,114 @@
+<?php date_default_timezone_set("Europe/Madrid");
+/*
+------------------------
+Langue: Español - España
+------------------------
+*/
+$cv_lang = "es-ES";
+$jeSuisEnEurope = "oui";
+
+$lang = array();
+
+$lang["parcourir_nav"] = "Recorrer Navigaci&oacute;n";
+
+$drapeauNational = 1;
+$lang['drapeau'] = "es";
+$lang['drapeau_alt'] = "Bandera de Espa&ntilde;a";
+$lang['drapeau_national'] = "eu";
+$lang['drapeau_national_alt'] = "Bandera de la Uni&oacute;n Europea";
+ 
+$lang['metadesc'] = "Grant Kiyoshi Mukai es un desarrollador Web y agente de comunicaciones en los Estados Unidos";
+$lang['PAGE_TITLE'] = 'Grant Mukai';
+$lang['NAME'] = 'Grant Kiyoshi Mukai';
+
+// Menu bar
+
+$lang['intro'] = 'Inicio';
+$lang['profile'] = 'Perfil';
+$lang['services'] = 'Servicios';
+$lang['web_cv'] = 'Desarrollo Web';
+$lang['pr_cv'] = 'Relaciones P&uacute;blicas';
+$lang['portfolio'] = 'Mi trabajo';
+$lang['contact'] = 'Contactarme';
+
+$lang['hello_1'] = "Hola, me llamo Grant Kiyoshi Mukai";
+$lang['hello_2'] = 'Desarrollador Web | Publicidad Correo Electr&oacute;nico | Administraci&oacute;n de Datos | Publicaciones | Relaciones P&uacute;blicas | Periodismo | Gesti&oacute;n de contenidos Web | Comunicaciones';
+$lang['hello_3'] = 'Desarrollador Web | Publicidad Correo Electr&oacute;nico | Administraci&oacute;n de Datos | Publicaciones';
+
+$lang['cookiesEurope'] = "Ley Europea sobre los cookies: Este sitio usa cookies para recordar que prefieres la versi&oacute;n espa&ntilde;ola de este sitio. Cuando est&aacute;s visitando mi sitio, te acceptas que va a haber cookies guardados en tu ordenador. Ninguna informaci&oacute;n personal no es enviada. Si tienes cuestiones sobre el uso de cookies, por favor <a href='#contact'>digame</a>.";
+
+$lang['select_country'] = "Elige su pa&iacute;s";
+
+$lang['skillset'] = 'Gama de competencias';
+
+$lang['social_profiles'] = 'Redes Sociales';
+
+$lang['bio'] = "
+<h3>Acerca de Mi</h3>
+
+<p>Me llamo Grant Kiyoshi Mukai y soy un Desarrollador Web y Especialista de servicios y sistemas Web a la <a href='http://www.apsanet.org'>Asociaci&oacute;n estadounidense de ciencia pol&iacute;tica</a> (APSA) en  los Estados Unidos de Am&eacute;rica. Trabajo en el Departamento de marketing y comunicaciones d&oacute;nde ayudo a mis colegios a gestionar el sitio p&uacute;blico de la asociaci&oacute;n y varios sitios WordPress destinados a promover la ciencia pol&iacute;tica y ayudar a nuestras secciones de intereses especializadas a organizarse en los redes. Asimismo, soy responsable de la gesti&oacute;n de bases de datos, anal&iacute;ticas estad&iacute;sticas y la generaci&oacute;n de formas de datos. Hago estas tareas proporcionando el servicio excelente al cliente en ingl&eacute;s, franc&eacute;s y espa&ntilde;ol. Aunque nuestras oficinas se encuentran en los Estados Unidos, tenemos miembros que vengan de todo el mundo.</p>
+
+<p>Antes de unirme a la APSA, trabajaba a la <a href='http://www.msb.edu'>Escuela de negocios McDonough</a> de la Universidad Georgetown en la capital del pa&iacute;s (mi-atl&aacute;ntico) y <a href='http://www.iqsolutions.com'>iQ Solutions</a> en Maryland, el estado vecino, desarrollando boletines electr&oacute;nicos. Algunos clientes de salud para qui&eacute;nes estaba trabajando incluyaban la Administraci&oacute;n de Salud Mental y Abuso de Sustancias de E.E.U.U. <em>(SAMHSA por sus siglas en ingl&eacute;s)</em> y los Institutos Nacionales de Salud <em>(NIH por sus siglas en ingl&eacute;s)</em>. Asimismo, he creado y actualizado paginas Web en ingl&eacute;s y en espa&ntilde;ol para el Instituto Nacional de Artritis y Enfermedades Musculoesquel&eacute;ticas y de la Piel <em>(NIAMS por sus siglas en ingl&eacute;s)</em>.</p>
+
+<p>Adem&aacute;s, muchas de mis responsabilidades ha incluido la verificaci&oacute;n de las p&aacute;ginas Web y documentos PDF para garantizar la conformidad a las normas establecidas por el W3C <em lang='en-CA'>(World Wide Web Consortium)</em>.</p>
+
+<p>Antes de mi cambio de carrero de periodismo al desarrollo Web y gesti&oacute;n de datos, era productor asociado de noticias al <i lang='en-US'><a href='http://www.wellesleymedia.org'>Wellesley Public Media</a></i></a> en el estado libre asociado de Massachusetts (norteste), d&oacute;nde estaba produciendo reportajes y documentarios para un canal de televisi&oacute;n. All&iacute;, he realizado contenido v&iacute;deo para la plataforma <a href='http://www.patch.com'>Patch.com</a>.</p>
+
+<p>A la universidad, he obtenido m&aacute;s experiencias en televisi&oacute;n gracias a mi pasant&iacute;a como aprendiz Web para Antiques Roadshow al WGBH Educational Foundation (PBS), el canal p&uacute;blico y no lucrativo de los Estados Unidos, y a <i lang='fr-FR'><a href='http://www.publicsynergies.com'>Publiques Synergies</a></i> y Rpublic.tv a Asni&egrave;res-sur-Seine, Francia, como aprendiz en v&iacute;deo y multimedia.</p>
+
+<p>He obtenido mi licenciatura en periodismo televisivo despu&eacute;s de cuatro a&ntilde;os de estudios a la <a href='http://www.bu.edu/com';>Escuela de comunicaciones</a> a la Universidad de Boston con especializaci&oacute;n en lengua y literatura francesa.</p>à
+
+<p>Yo paso mi tiempo libre bailando el Lindy Hop y Blues, dos tipos de baile popular en los Estados Unidos quienes han cruzado el atl&aacute;ntico. Las espa&ntilde;olas y los espa&ntilde;oles se diverten de este pasatiempo en comunidades de baile en Barcelona. Me gusta tambi&eacute;n conocer otras culturas e idiomas.</p>
+";
+
+$lang['derniere_maj'] = "&Uacute;ltima actualizaci&oacute;n";
+
+$lang['web_development'] = "Desarrollo Web";
+$lang['public_relations'] = "Relaciones P&uacute;blicas";
+$lang['experience'] = "Experiencias Profesionales";
+$lang['duties'] = "Tareas";
+$lang['education'] = "Educaci&oacute;n";
+
+$lang['download_webcv'] = "Descargar CV";
+$webFileDate = new DateTime('05/09/2016');
+$lang['webfile_size'] = 3; //2,7 MB
+
+//$lang['download_prcv'] = "Descargar CV";
+//$prFileDate = new DateTime('04/21/2016');
+//$lang['prfile_size'] = 72;
+
+$lang['ce_que_je_peux_faire_titre'] = "Lo Que Puedo Hacer";
+$lang['redaction_soustitre'] = "Escritura";
+$lang['redaction_p'] = "Porque estudi&oacute; el periodismo, yo se como bien escribir para las publicaciones escritas y utiliz&oacute; mis competencias para reportar las noticias y tambi&eacute;n para acentuar las empresas peque&ntilde;as en el espacio de promoci&oacute;n.";
+$lang['multimedia_soustitre'] = "Multimedia";
+$lang['multimedia_p'] = "En mi otra vida, trabaj&oacute; como periodista en la televisi&oacute;n y editor de v&iacute;deos. Realiz&oacute; mis propios reportajes del inicio hasta la fin y produzc&oacute; mis propios efectos especiales.";
+$lang['courriel_soustitre'] = "Dise&ntilde;o Correo Electr&oacute;nico";
+$lang['courriel_p'] = "Compreniendo que el dise&ntilde;o de correo electr&oacute;nico no es el mismo que el dise&ntilde;o Web, soy competente con el c&oacute;digo &ldquo;obsoleto&rdquo; del HTML y CSS entre los par&eacute;ntesis. Asimismo, tengo una buena idea de lo que Outlook acepta y no acepta.";
+$lang['codepropre_soustitre'] = "C&oacute;digo Limpio";
+$lang['codepropre_p'] = "Estoy serioso cuando escribo c&oacute;digo HTML5, CSS3 y PHP5 el m&aacute;s limpio y actualizado. Mi c&oacute;digo es correctamente sangrado y sigue los est&aacute;ndares establicidos por el W3C.";
+$lang['adaptatif_soustitre'] = "Dise&ntilde;o Web Adaptable";
+$lang['adaptatif_p'] = "No solo comprendo la implementaci&oacute;n de <em>media quieres</em> para flotar y escalar contenidos para el dise&ntilde;o Web adaptable, comprendo tambi&eacute;n las mejores pr&aacute;cticas para crear un correo electr&oacute;nico adaptable (cuando apoyado)"; 
+$lang['soutien_soustitre'] = "Apoyo";
+$lang['soutien_p'] = "Aunque proporciono servicios Web para mi asociaci&oacute;n, ofrezco tambi&eacute;n entrenamiento a nuestros empleados c&oacute;mo pueden administrar sus propios contenidos y ayudo a nuestros miembros en ingl&eacute;s, franc&eacute;s; y espa&ntilde;ol con preguntas de membres&iacute;a.";
+
+$lang['status_title'] = 'Mi Estado';
+$lang['status_1'] = "Estoy trabajando para la Asociaci&oacute;n estadounidense de ciencia pol&iacute;tica";
+$lang['status_2'] = "Trabajo en el Departamento de comunicaciones y marketing con un equipo fantastico.";
+$lang['status_3'] = 'Cont&aacute;ctame';
+
+$lang['contact_title'] = 'Cont&aacute;ctame';
+$lang['contact_subtitle'] = "Me da una l&iacute;nea o llamame por tel&eacute;fono. Me gustar&iacute;a escucharle.";
+$lang['contact_paragraph'] = "Puede contactarme utilizando una de las maneras abajo<span class='formuleDroite'> o el formulario de contacto a su derecho</span><span class='formuleBas' style='display:none;'>abajo</span>.";
+
+$lang['customers'] = "Trabaj&oacute; para estas empresas";
+
+$lang['formule_nom'] = "Nombre";
+$lang['formule_courriel'] = "Dirrecci&oacute;n de correo electr&oacute;nico";
+$lang['formule_message'] = "Su mensaje";
+$lang['formule_envoyer'] = "Envialo";
+
+$lang['copyright'] = "Grant Mukai. Todos los derechos reservados. Iconos de banderas graciosamente proporcionados por <a href='http://www.icondrawer.com'>Icon Drawer</a>.";
+$lang['creditdelaphoto'] = "Cr&eacute;dito de Foto Arriba";
+$lang['par'] = "por";
+$lang['license2photo'] = "Autorizada bajo el licencia Creative Commons. No hay modificaciones a esta foto.";
+?>
