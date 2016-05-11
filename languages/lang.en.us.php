8e6ea1f4a70824e7fb1df20d81a543e5ee4762b3
grantmukai <grantmukai@users.noreply.github.com>
2016-05-11 14:26:06 -0400

    US English Intro + Web CV

--- languages/lang.en.us.php
+++ languages/lang.en.us.php
@@ -48,13 +48,15 @@ $lang['bio'] = "
 
 <p>Before joining APSA, I worked at both the <a href='http://msb.georgetown.edu'>Georgetown University McDonough School of Business</a> in Washington and <a href='http://www.iqsolutions.com'>iQ Solutions</a> in Rockville, Maryland developing email newsletters. Some of the healthcare agencies I worked for include the Substance Abuse and Mental Health Services Administration (SAMHSA) and the National Institutes of Health (NIH). I have also produced and updated English and Spanish webpages for the National Institutes of Arthritis and Musculoskeletal and Skin Diseases (NIAMS).</p>
 
-<p>A large part of my responsibilities consisted of making sure that webpages and documents meet the Web Accessibility Guidelines set forth by the World Wide Web Consortium and Section 508 of the Americans with Disabilities Act.</p>
+<p>A large part of my responsibilities at iQ Solutions consisted of making sure that webpages and documents met the Web Accessibility Guidelines set forth by the World Wide Web Consortium and Section 508 of the Americans with Disabilities Act.</p>
 
 <p>Prior to my career switch from journalism to Web development, I was an Associate News Producer for <a href='http://www.wellesleymedia.org'>Wellesley Public Media</a> in the town of Wellesley, Massachusetts where I produced news reports and documentaries for the local cable access station. In the Boston area, I was instrumental in bringing online video content to the Patch.com platform where I provided freelance video work on a number of local events and businesses and ran a weekly real estate column.</p>
 
-<p>In college, I gained additional television experiences from the <a href='http://www.wgbh.org'>WGBH Educational Foundation (PBS)</a> as the Antiques Roadshow Web Intern and with <a href='http://www.publicsynergies.com'>Public Synergies</a> and Rpublictv in Asni&egrave;res-sur-Seine, France, as a multimedia and video intern.</p>
+<p>In college, I gained additional television experiences from the <a href='http://www.wgbh.org'>WGBH Educational Foundation (PBS)</a> as the Antiques Roadshow Web Intern and with <a href='http://www.publicsynergies.com'>Public Synergies</a> and Rpublictv in Asni&egrave;res-sur-Seine, France as a multimedia and video intern.</p>
 
 <p>I am a proud alumnus of Boston University's <a href='http://www.bu.edu/com'>College of Communication</a> where I graduated with a Bachelor of Science in Broadcast Journalism and a minor in French Language and Literature.</p>
+
+<p>In my free time, I can be found dancing Lindy Hop and Blues, two popular styles of partner dance, and hanging out in Washington's international scene.</p>
 ";
 
 $lang['derniere_maj'] = "Last updated";
@@ -66,8 +68,8 @@ $lang['duties'] = "Duties";
 $lang['education'] = "Education";
 
 $lang['download_webcv'] = "Download CV";
-$webFileDate = new DateTime('04/08/2016');
-$lang['webfile_size'] = 77;
+$webFileDate = new DateTime('05/11/2016');
+$lang['webfile_size'] = 53;
 
 $lang['download_prcv'] = "Download CV";
 $prFileDate = new DateTime('04/11/2016');
