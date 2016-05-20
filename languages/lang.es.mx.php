b44c35871b86cfbc121e8663b96d74fbacfcccfd
Kiyoshi Mukai <hello@kiyoshimukai.com>
2016-05-20 19:26:48 -0400

    MX: Intro + cv web

--- languages/lang.es.mx.php
+++ languages/lang.es.mx.php
@@ -17,9 +17,9 @@ $drapeauNational = 0;
 $lang['drapeau'] = "mx";
 $lang['drapeau_alt'] = "Bandera de M&eacute;xico";
  
-$lang['metadesc'] = "Grant Kiyoshi Mukai es un desarrollador Web y agente de comunicaciones a Washington, Distrito de Columbia, Estados Unidos";
-$lang['PAGE_TITLE'] = 'Grant Mukai';
-$lang['NAME'] = 'Grant Kiyoshi Mukai';
+$lang['metadesc'] = "Kiyoshi Mukai es un desarrollador Web y agente de comunicaciones a Washington, Distrito de Columbia, Estados Unidos";
+$lang['PAGE_TITLE'] = 'Kiyoshi Mukai';
+$lang['NAME'] = 'Kiyoshi Mukai';
 
 // Menu bar
 
@@ -31,7 +31,7 @@ $lang['pr_cv'] = 'Relaciones P&uacute;blicas';
 $lang['portfolio'] = 'Mi trabajo';
 $lang['contact'] = 'Contactarme';
 
-$lang['hello_1'] = "Hola, me llamo Grant Kiyoshi Mukai";
+$lang['hello_1'] = "Hola, me llamo Kiyoshi Mukai";
 $lang['hello_2'] = 'Desarrollador Web | Publicidad Correo Electr&oacute;nico | Administraci&oacute;n de Datos | Publicaciones | Relaciones P&uacute;blicas | Periodismo | Gesti&oacute;n de contenidos Web | Comunicaciones';
 $lang['hello_3'] = 'Desarrollador Web | Publicidad Correo Electr&oacute;nico | Administraci&oacute;n de Datos | Publicaciones';
 
@@ -44,9 +44,9 @@ $lang['social_profiles'] = 'Redes Sociales';
 $lang['bio'] = "
 <h3>Acerca de Mi</h3>
 
-<p>Me llamo Grant Kiyoshi Mukai y soy un Desarrollador Web y Especialista de servicios y sistemas Web a la <a href='http://www.apsanet.org'>Asociaci&oacute;n estadounidense de ciencia pol&iacute;tica</a> (APSA) en el Distrito de Columbia, Estados Unidos de Am&eacute;rica. Trabajo en el Departamento de promoci&oacute;n y comunicaciones d&oacute;nde ayudo a mis colegios a gestionar el sitio p&uacute;blico de la asociaci&oacute;n y varios sitios WordPress destinados a promover la ciencia pol&iacute;tica y ayudar a nuestras secciones de intereses especializadas a organizarse en los redes. Asimismo, soy responsable de la gesti&oacute;n de bases de datos, anal&iacute;ticas estad&iacute;sticas, y la generaci&oacute;n de formas de datos. Hago estas tareas proporcionando el servicio excelente al cliente en ingl&eacute;s, franc&eacute;s, y espa&ntilde;ol. Aunque nuestras oficinas se encuentran en los Estados Unidos, tenemos miembros que vengan de todo el mundo.</p>
+<p>Me llamo Kiyoshi Mukai y soy un Desarrollador Web y Especialista de servicios y sistemas Web a la <a href='http://www.apsanet.org'>Asociaci&oacute;n estadounidense de ciencia pol&iacute;tica</a> (APSA) en el Distrito de Columbia, Estados Unidos de Am&eacute;rica. Trabajo en el Departamento de promoci&oacute;n y comunicaciones d&oacute;nde ayudo a mis colegios a gestionar el sitio p&uacute;blico de la asociaci&oacute;n y varios sitios WordPress destinados a promover la ciencia pol&iacute;tica y ayudar a nuestras secciones de intereses especializadas a organizarse en los redes. Asimismo, soy responsable de la gesti&oacute;n de bases de datos, anal&iacute;ticas estad&iacute;sticas, y la generaci&oacute;n de formas de datos. Hago estas tareas proporcionando el servicio excelente al cliente en ingl&eacute;s, franc&eacute;s, y espa&ntilde;ol. Aunque nuestras oficinas se encuentran en los Estados Unidos, tenemos miembros que vengan de todo el mundo.</p>
 
-<p>Antes de unirme a la APSA, trabajaba a la <a href='http://www.msb.edu'>Escuela de negocios McDonough</a> de la Universidad Georgetown en la ciudad capital de Washington y <a href='http://www.iqsolutions.com'>iQ Solutions</a> en Maryland, el estado vecino, desarrollando boletines electr&oacute;nicos. Algunos clientes de salud para qui&eacute;nes estaba trabajando incluyaban la Administraci&oacute;n de Salud Mental y Abuso de Sustancias de E.E.U.U. <em>(SAMHSA por sus siglas en ingl&eacute;s)</em> y los Institutos Nacionales de Salud <em>(NIH por sus siglas en ingl&eacute;s)</em>. Asimismo, cre&oacute; y actualiz&oacute; paginas Web en ingl&eacute;s y en espa&ntilde;ol para el Instituto Nacional de Artritis y Enfermedades Musculoesquel&eacute;ticas y de la Piel <em>(NIAMS por sus siglas en ingl&eacute;s)</em>.</p>
+<p>Antes de unirme a la APSA, trabajaba a la <a href='http://www.msb.edu'>Escuela de negocios McDonough</a> de la Universidad Georgetown en la ciudad capital de mi Distrito y <a href='http://www.iqsolutions.com'>iQ Solutions</a> en Maryland, el estado vecino, desarrollando boletines electr&oacute;nicos. Algunos clientes de salud para qui&eacute;nes estaba trabajando incluyaban la Administraci&oacute;n de Salud Mental y Abuso de Sustancias de E.E.U.U. <em>(SAMHSA por sus siglas en ingl&eacute;s)</em> y los Institutos Nacionales de Salud <em>(NIH por sus siglas en ingl&eacute;s)</em>. Asimismo, cre&oacute; y actualiz&oacute; paginas Web en ingl&eacute;s y en espa&ntilde;ol para el Instituto Nacional de Artritis y Enfermedades Musculoesquel&eacute;ticas y de la Piel <em>(NIAMS por sus siglas en ingl&eacute;s)</em>.</p>
 
 <p>Adem&aacute;s, muchas de mis responsabilidades incluid&aacute; la verificaci&oacute;n de las p&aacute;ginas Web y documentos PDF para garantizar la conformidad a las normas establecidas por el W3C <em lang='en-CA'>(World Wide Web Consortium)</em>.</p>
 
@@ -55,6 +55,14 @@ $lang['bio'] = "
 <p>A la universidad, obtuv&oacute; m&aacute;s experiencias en televisi&oacute;n gracias a mi pasant&iacute;a como aprendiz Web para Antiques Roadshow al WGBH Educational Foundation (PBS), el canal p&uacute;blico y no lucrativo de los Estados Unidos, y a <i lang='fr-FR'><a href='http://www.publicsynergies.com'>Publiques Synergies</a></i> y Rpublic.tv a Asni&egrave;res-sur-Seine, Francia, como aprendiz en v&iacute;deo y multimedia.</p>
 
 <p>Obtuv&oacute; mi licenciatura en periodismo televisivo despu&eacute;s de cuatro a&ntilde;os de estudios a la <a href='http://www.bu.edu/com';>Escuela de comunicaciones</a> a la Universidad de Boston con especializaci&oacute;n en lengua y literatura francesa.</p>
+
+<p>En mi tiempo libre, el baile es algo muy importante en mi vida. Los dos tipos de baile que hago son el Lindy Hop y Blues. Hay una peque&ntilde;a comunidad de Lindy Hop en la Ciudad de M&eacute;xico. Me gusta tambi&eacute;n la comunidad internacional de Washington porque me da la opportunidad de practicar mi espa&ntilde;ol.</p>
+
+<p>Cordialemente,</p>
+
+<img src='assets/images/signatures/signature_kiyoshimukai.png' alt='Kiyoshi Mukai' style='width:40%;height:40%;' />
+
+<p><strong>Kiyoshi Mukai</strong></p>
 ";
 
 $lang['derniere_maj'] = "&Uacute;ltima actualizaci&oacute;n";
@@ -66,8 +74,8 @@ $lang['duties'] = "Tareas";
 $lang['education'] = "Educaci&oacute;n";
 
 $lang['download_webcv'] = "Descargar CV";
-$webFileDate = new DateTime('04/19/2016');
-$lang['webfile_size'] = 54;
+$webFileDate = new DateTime('05/20/2016');
+$lang['webfile_size'] = 78;
 
 $lang['download_prcv'] = "Descargar CV";
 $prFileDate = new DateTime('04/21/2016');
