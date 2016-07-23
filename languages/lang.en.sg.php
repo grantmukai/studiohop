04068f0b5b9dcf78ae25f5333d811aa1fc719288
Kiyoshi Mukai <hello@kiyoshimukai.com>
2016-07-22 20:08:27 -0400

    Singapore started...

---
+++ languages/lang.en.sg.php
@@ -0,0 +1,118 @@
+<?php date_default_timezone_set("Asia/Singapore");
+/*
+-----------------------------
+Language: English - Singapore
+-----------------------------
+*/
+$cv_lang = "en-SG";
+$jeSuisEnEurope = "non";
+ 
+$lang = array();
+
+$lang["parcourir_nav"] = "Toggle Navigation";
+
+$drapeauNational = 0;
+$lang['drapeau'] = "sg";
+$lang['drapeau_alt'] = "Flag of Singapore";
+ 
+$lang['metadesc'] = "Kiyoshi Mukai is a Web services and systems support specialist in the United States";
+$lang['PAGE_TITLE'] = 'Kiyoshi Mukai';
+$lang['NAME'] = 'Kiyoshi Mukai';
+
+// Menu bar
+
+$lang['intro'] = 'Home';
+$lang['profile'] = 'Profile';
+$lang['services'] = 'Services';
+$lang['web_cv'] = 'Web Development';
+$lang['pr_cv'] = 'Communications';
+$lang['portfolio'] = 'My Work';
+$lang['contact'] = 'Contact Me';
+
+$lang['hello_1'] = "Hello, I'm Kiyoshi Mukai<br /><small>Hello, nama saya Kiyoshi Mukai<br />&#20320;&#22909;&#25105;&#30340;&#21517;&#23383;&#26159;&#21521;&#20117;&#28165;<br />
+வணக்கம் என் பெயர் கியோஷி ஆகும்</small>";
+$lang['hello_2'] = 'Web Developer';
+$lang['hello_3'] = 'Web Developer';
+
+$lang['skillset'] = 'Skillset';
+
+// COOKIES!
+//$lang['cookiesJeComprends'] = "I understand";
+//$lang['cookiesMerci'] = "Thank You!";
+
+//$lang['cookiesEurope'] = "European Cookie Law: This site uses cookies to remember that you prefer the UK version of this site. By visiting this site, you agree to allowing cookies to be stored on your computer. No personal information is being collected. If you have questions, please <a href='#contact'>contact me</a>.";
+
+$lang['select_country'] = "Select your country";
+
+$lang['social_profiles'] = 'Social Profiles';
+
+$lang['bio'] = "
+<h3>Who Am I?</h3>
+
+<p>My name is Kiyoshi Mukai, also known as Grant, and I am a Web developer and systems support specialist for the <a href='http://www.apsanet.org'>American Political Science Association</a> in the United States of America. I work in the Marketing and Communications Department where I help manage our association's public-facing site as well as several small-scale WordPress sites devoted to promoting political science and helping our special interest sections organise themselves on the Web. I also help out with a lot of database management, which includes building SQL queries and generating reports, while providing excellent customer service to our members in English, French and Spanish. Although we are located in the United States, we have a very large international membership.</p>
+
+<p>Before joining the APSA, I worked at both the <a href='http://msb.georgetown.edu'>Georgetown University McDonough School of Business</a> in the District of Columbia (Mid-Atlantic region) and <a href='http://www.iqsolutions.com'>iQ Solutions</a>, in the State of Maryland just north of the District of Columbia, developing email newsletters. Some of the healthcare clients I worked for included the Substance Abuse and Mental Health Services Administration (SAMHSA) and the National Institutes of Health (NIH). I have also produced and updated English and Spanish webpages by coding for the National Institutes of Arthritis and Musculoskeletal and Skin Diseases (NIAMS).</p>
+
+<p>A large part of my responsibilities have comprised making sure that webpages and documents meet the Web Accessibility Guidelines set forth by the World Wide Web Consortium.</p>
+
+<p>Prior to my career switch from journalism to Web development and database management, I was an Associate Producer for <a href='http://www.wellesleymedia.org'>Wellesley Public Media</a> in the Commonwealth of Massachusetts (Northeast region) where I produced news reports and documentaries for the local cable access station. In this same area, I was instrumental in bringing online video content to Patch.com, an American online news platform, where I provided freelance video work on a number of local events and businesses and ran a weekly real estate column.</p>
+
+<p>I am a proud graduate of Boston University's <a href='http://www.bu.edu/com'>College of Communication</a> where I graduated with a Bachelor of Science in Broadcast Journalism and an academic minor in French Language and Literature. In the United States, an academic minor is a field of study meant to compliment the bachelor's degree.</p>
+
+<p>I hope to hear from you soon,</p>
+
+<img src='assets/images/signatures/signature_grantmukai.png' alt='Kiyoshi Mukai&apos;s Signature' style='width:40%;height:40%;' />
+
+<p><strong>Kiyoshi Mukai (Grant)</strong></p>;";
+
+$lang['derniere_maj'] = "Last updated";
+
+$lang['web_development'] = "Web Development";
+$lang['public_relations'] = "Communications";
+$lang['experience'] = "Work Experience";
+$lang['duties'] = "Duties";
+$lang['education'] = "Education";
+
+$lang['download_webcv'] = "Download CV";
+$webFileDate = new DateTime('07/22/2016');
+$lang['webfile_size'] = 166;
+
+//$lang['download_prcv'] = "Download CV";
+//$prFileDate = new DateTime('07/13/2016');
+//$lang['prfile_size'] = 76;
+
+$lang['ce_que_je_peux_faire_titre'] = "What I Can Do";
+$lang['redaction_soustitre'] = "Writing";
+$lang['redaction_p'] = "As a journalism major, I know how to write well for both print and online publications. I have used my writing abilities to report the news and also to promote businesses in the marketing space.";
+$lang['multimedia_soustitre'] = "Multimedia";
+$lang['multimedia_p'] = "In my former days, I worked as a television reporter and video editor. I produced my own stories from start to finish and created my own special effects.";
+$lang['courriel_soustitre'] = "Email Design";
+$lang['courriel_p'] = "Understanding that email design is not the same as Web design, I am proficient in coding &ldquo;depricated&rdquo; HTML and inline CSS as well as having a good idea of what Outlook does and does not support.";
+$lang['codepropre_soustitre'] = "Clear Code";
+$lang['codepropre_p'] = "I am very serious about only writing the cleanest up-to-date HTML5, CSS3 and PHP7. My code is properly indented and follows standards set by the W3C.";
+$lang['adaptatif_soustitre'] = "Responsive Design";
+$lang['adaptatif_p'] = "Not only do I understand how to implement media queries and float and scale content for responsive Web design, I also undestand best practices for creating responsive email (where supported).";
+$lang['soutien_soustitre'] = "Support";
+$lang['soutien_p'] = "Although I provide Web services for my association, I also offer training to our staff on how to manage their own content and assist our members and staff in English, French and Spanish with website training and membership issues.";
+
+$lang['status_title'] = 'My status';
+$lang['status_1'] = "I currently work for the American Political Science Association";
+$lang['status_2'] = "I work in the Communications &amp; Marketing Department helping our members better connect to the field of political science.";
+$lang['status_3'] = 'Contact me';
+
+$lang['contact_title'] = 'Contact Me';
+$lang['contact_subtitle'] = "Drop me a line or give me a ring. I'd love to hear from you";
+$lang['contact_paragraph'] = "Feel free to contact me using one of the methods below<span class='formuleDroite'> or use the contact form on the right</span><span class='formuleBas' style='display:none;'>below</span>.";
+
+$lang['customers'] = 'Companies I have worked for';
+
+$lang['formule_nom'] = "Name";
+$lang['formule_courriel'] = "Email Address";
+$lang['formule_message'] = "Your message";
+$lang['formule_envoyer'] = "Send It";
+
+$lang['copyright'] = "Kiyoshi Mukai (Grant). All rights reserved. Flag icons graciously provided by <a href='http://www.icondrawer.com'>Icon Drawer</a>.";
+//$lang['creditdelaphoto'] = "Main Photo Credit";
+//$lang['par'] = "by";
+//$lang['license2photo'] = "Licensed under the Creative Commons License. No modifications have been applied to this photo.";
+?>
