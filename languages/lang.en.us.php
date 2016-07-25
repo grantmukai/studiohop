0f6db1041b6f6d2f8135cdb16f43dbeca6339e52
grantmukai <grantmukai@users.noreply.github.com>
2016-07-25 15:16:06 -0400

    Additional English US CV changes

--- languages/lang.en.us.php
+++ languages/lang.en.us.php
@@ -76,7 +76,7 @@ $lang['download_webcv'] = "Download Web CV &mdash; PDF";
 $webFileDate = new DateTime('07/23/2016');
 $lang['webfile_size'] = 78;
 
-$lang['download_prcv'] = "Download CV";
+$lang['download_prcv'] = "Download Communications CV &mdash; PDF";
 $prFileDate = new DateTime('07/25/2016');
 $lang['prfile_size'] = 79;
 
@@ -88,7 +88,7 @@ $lang['multimedia_p'] = "In my reporter days, I worked as a television producer 
 $lang['courriel_soustitre'] = "Email Design";
 $lang['courriel_p'] = "By understanding that email design is not the same as Web design, I am proficient in coding &ldquo;depricated&rdquo; HTML and inline CSS as well as having a good idea of what Outlook does and does not support.";
 $lang['codepropre_soustitre'] = "Clear Code";
-$lang['codepropre_p'] = "I am very serious about only writing the cleanest up-to-date HTML5, CSS3 and PHP5. My code is properly indented and follows standards set by the W3C and Section 508 of the Americans with Disabilities Act.";
+$lang['codepropre_p'] = "I am very serious about only writing the cleanest up-to-date HTML5, CSS3 and PHP7. My code is properly indented and follows standards set by the W3C and Section 508 of the Americans with Disabilities Act.";
 $lang['adaptatif_soustitre'] = "Responsive Design";
 $lang['adaptatif_p'] = "Not only do I understand how to implement media queries and float and scale content for responsive Web design, I also undestand best practices for creating responsive email (where supported).";
 $lang['soutien_soustitre'] = "Support";
