15de815f3879813b699fcc13fe0242a3338aaf3f
Kiyoshi Mukai <hello@kiyoshimukai.com>
2016-11-25 12:55:13 -0500

    coffee nature

--- languages/lang.fr.be.php
+++ languages/lang.fr.be.php
@@ -85,7 +85,7 @@ $webFileDate = new DateTime('11/24/2016');
 $lang['webfile_size'] = 3; //2.7MB
 
 $lang['telecharger_comcv'] = "T&eacute;l&eacute;charger CV &mdash; PDF";
-$prFileDate = new DateTime('10/20/2016');
+$prFileDate = new DateTime('11/25/2016');
 $lang['prfile_size'] = 3; //2.7MB
 
 $lang['ce_que_je_peux_faire_titre'] = "Ce Que Je Peux Faire";
