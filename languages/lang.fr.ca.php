7aab89ee64af200ac0d3327b087037b9de3e097c
Kiyoshi Mukai <hello@kiyoshimukai.com>
2017-02-03 19:54:37 -0500

    québec web

--- languages/lang.fr.ca.php
+++ languages/lang.fr.ca.php
@@ -15,103 +15,103 @@ $villeMax = 6;
 
 $drapeauNational = 1;
 $lang['drapeau'] = "qc";
-$lang['drapeau_alt'] = "Drapeau du Qu&eacute;bec";
+$lang['drapeau_alt'] = "Drapeau du Québec";
 $lang['drapeau_national'] = "ca";
 $lang['drapeau_national_alt'] = "Drapeau du Canada";
 
-$lang['metadesc'] = "Grant Muka&iuml; est d&eacute;veloppeur Web et agent de communications &agrave; Washington, District de Columbia"; 
-$lang['PAGE_TITLE'] = 'Grant Muka&iuml;';
-$lang['NAME'] = 'Grant Muka&iuml;';
+$lang['metadesc'] = "Grant Mukaï est développeur Web et agent de communications à Washington, District de Columbia"; 
+$lang['PAGE_TITLE'] = 'Grant Mukaï';
+$lang['NAME'] = 'Grant Mukaï';
 
 // Menu bar
 
 $lang['intro'] = 'Accueil';
 $lang['profile'] = 'Profil';
 $lang['services'] = 'Services';
-$lang['web_cv'] = 'D&eacute;veloppement Web';
+$lang['web_cv'] = 'Développement Web';
 $lang['com_cv'] = 'Communications';
 $lang['portfolio'] = 'Mon travail';
 $lang['contact'] = 'Contactez-Moi';
 
-$lang['hello_1'] = "Bonjour, mon nom est Grant Muka&iuml;";
-$lang['hello_2'] = "D&eacute;veloppement Web | Courriels de Marketing | Administration des Donn&eacute;es | Multim&eacute;dia | Communications | Promotion | Gestion des Contenu Web | Journalisme";
-$lang['hello_3'] = "D&eacute;veloppement Web | Courriels de Marketing | Administration de Donn&eacute;es | Multim&eacute;dia";
+$lang['hello_1'] = "Bonjour, mon nom est Grant Mukaï";
+$lang['hello_2'] = "Développement Web | Courriels de Marketing | Administration des Données | Multimédia | Communications | Promotion | Gestion des Contenu Web | Journalisme";
+$lang['hello_3'] = "Développement Web | Courriels de Marketing | Administration de Données | Multimédia";
 
 $lang['select_country'] = "Choisissez votre pays";
 
-$lang['skillset'] = 'Gamme de comp&eacute;tences';
+$lang['skillset'] = 'Gamme de compétences';
 
-$lang['social_profiles'] = 'R&eacute;seaux Sociaux';
+$lang['social_profiles'] = 'Réseaux Sociaux';
 
 $lang['bio'] = "
 <h3>Qui Je Suis</h3>
 
-<p>Je m'appelle Grant Muka&iuml; et je suis D&eacute;veloppeur Web et sp&eacute;cialiste en services et syst&egrave;mes &agrave; <a href='http://www.apsanet.org/'>l'Association am&eacute;ricaine de science politique</a> (APSA) au District de Columbia, &Eacute;tats-Unis. Je travaille dans le D&eacute;partement de marketing et communications o&ugrave; j'aide mes co&eacute;quipiers &agrave; g&eacute;rer le site public de l'association ainsi que plusieurs sites WordPress consacr&eacute;s &agrave; la promotion de la science politique et qui aident nos sections d'int&eacute;r&ecirc;ts sp&eacute;ciaux &agrave; s'organiser aux r&eacute;seaux. Je suis aussi responsable de la gestion des bases de donn&eacute;es, de l'analyse statistique et la g&eacute;n&eacute;ration des tableaux de donn&eacute;es tout en fournissant de l'excellent service &agrave; la client&egrave;le en anglais, fran&ccedil;ais et espagnol. Bien que nos bureaux se trouvent aux &Eacute;tats-Unis, nous avons des membres provenant des quatre coins du monde.</p>
+<p>Je m'appelle Grant Mukaï et je suis Développeur Web et spécialiste en services et systèmes à <a href='http://www.apsanet.org/'>l'Association américaine de science politique</a> (APSA) au District de Columbia, États-Unis. Je travaille dans le Département de marketing et communications o&ugrave; j'aide mes coéquipiers à gérer le site public de l'association ainsi que plusieurs sites WordPress consacrés à la promotion de la science politique et qui aident nos sections d'intér&ecirc;ts spéciaux à s'organiser aux réseaux. Je suis aussi responsable de la gestion des bases de données, de l'analyse statistique et la génération des tableaux de données tout en fournissant de l'excellent service à la clientèle en anglais, fran&ccedil;ais et espagnol. Bien que nos bureaux se trouvent aux États-Unis, nous avons des membres provenant des quatre coins du monde.</p>
 
-<p>Avant de me rejoindre &agrave; l'APSA, j'ai travaill&eacute; &agrave; <a href='http://msb.georgetown.edu'>l'&Eacute;cole McDonough de commerce</a> &agrave; l'Universit&eacute; Georgetown &agrave; Washington et <a href='http://www.iqsolutions.com'>iQ Solutions</a> &agrave; Rockville, Maryland, en d&eacute;veloppant des bulletins &eacute;lectroniques. Quelques clients de sant&eacute; pour lesquels j'ai travaill&eacute; ont compris l'Administration pour abus des stup&eacute;fiants et services sant&eacute; mentale <em>(SAMHSA en anglais)</em> et les Instituts nationaux de la sant&eacute; <em>(NIH en anglais)</em>. J'ai aussi r&eacute;dig&eacute; et mit &agrave; jour des pages Web en anglais et en espagnol pour les Instituts nationaux de l'arthrite, affections musculo-squelettiques et maladies de la peau <em>(NIAMS en anglais)</em>.</p>
+<p>Avant de me rejoindre à l'APSA, j'ai travaillé à <a href='http://msb.georgetown.edu'>l'École McDonough de commerce</a> à l'Université Georgetown à Washington et <a href='http://www.iqsolutions.com'>iQ Solutions</a> à Rockville, Maryland, en développant des bulletins électroniques. Quelques clients de santé pour lesquels j'ai travaillé ont compris l'Administration pour abus des stupéfiants et services santé mentale <em>(SAMHSA en anglais)</em> et les Instituts nationaux de la santé <em>(NIH en anglais)</em>. J'ai aussi rédigé et mit à jour des pages Web en anglais et en espagnol pour les Instituts nationaux de l'arthrite, affections musculo-squelettiques et maladies de la peau <em>(NIAMS en anglais)</em>.</p>
 
-<p>Une grande partie de mes responsabilit&eacute;s a inclut la v&eacute;rification des pages Web et documents PDF afin d'assurer la conformit&eacute; aux normes &eacute;tablies par le W3C <em>(World Wide Web Consortium)</em>.</p>
+<p>Une grande partie de mes responsabilités a inclut la vérification des pages Web et documents PDF afin d'assurer la conformité aux normes établies par le W3C <em>(World Wide Web Consortium)</em>.</p>
 
-<p>Avant mon changement de carri&egrave;re du journalisme au d&eacute;veloppement Web et communications, j'&eacute;tais r&eacute;alisateur adjoint d'actualit&eacute; chez <a href='http://www.wellesleymedia.org'>Wellesley Public Media</a> dans le village de Wellesley, Massachusetts, o&ugrave; je produisais des court m&eacute;trages et documentaires pour une cha&icirc;ne de t&eacute;l&eacute;vision. Dans la m&eacute;tropole de Boston, j'ai apport&eacute; du contenu vid&eacute;o au plateforme Patch.com.</p>
+<p>Avant mon changement de carrière du journalisme au développement Web et communications, j'étais réalisateur adjoint d'actualité chez <a href='http://www.wellesleymedia.org'>Wellesley Public Media</a> dans le village de Wellesley, Massachusetts, o&ugrave; je produisais des court métrages et documentaires pour une cha&icirc;ne de télévision. Dans la métropole de Boston, j'ai apporté du contenu vidéo au plateforme Patch.com.</p>
 
-<p>J'ai obtenu un baccalaur&eacute;at en sciences en journalisme t&eacute;l&eacute;vis&eacute; &agrave; <a href='http://www.bu.edu/com'>l'&Eacute;cole de communications</a> &agrave; l'Universit&eacute; de Boston avec une mineure en langue et litt&eacute;rature fran&ccedil;aise.</p>
+<p>J'ai obtenu un baccalauréat en sciences en journalisme télévisé à <a href='http://www.bu.edu/com'>l'École de communications</a> à l'Université de Boston avec une mineure en langue et littérature fran&ccedil;aise.</p>
 
-<p>Pendant mon temps libre, je suis danseur de Lindy Hop et Blues, deux styles de danse sociale assez populaires partout au Canada et surtout &agrave; Montr&eacute;al, la capitale de danse au pays. Je suis &agrave; Montr&eacute;al plusieurs fois par ann&eacute;e pour y participer. Je suis aussi tr&egrave;s actif dans la sc&egrave;ne internationale &agrave; Washington et je suis en train d'am&eacute;liorer mes comp&eacute;tences en espagnol afin de pouvoir voyager au Mexique.</p>
+<p>Pendant mon temps libre, je suis danseur de Lindy Hop et Blues, deux styles de danse sociale assez populaires partout au Canada et surtout à Montréal, la capitale de danse au pays. Je suis à Montréal plusieurs fois par année pour y participer. Je suis aussi très actif dans la scène internationale à Washington et je suis en train d'améliorer mes compétences en espagnol afin de pouvoir voyager au Mexique.</p>
 
 <p>Cordialement,</p>
 
 <img src='assets/images/signatures/signature_grantmukai.png' alt='Grant Mukai&apos;s Signature' style='width:40%;height:40%;' />
 
-<p><strong>Grant Muka&iuml;</strong></p>";
+<p><strong>Grant Mukaï</strong></p>";
 
-$lang['derniere_maj'] = "Derni&egrave;re mise &agrave; jour";
+$lang['derniere_maj'] = "Dernière mise à jour";
 
-$lang['web_development'] = "D&eacute;veloppement Web";
+$lang['web_development'] = "Développement Web";
 $lang['communications'] = "Communications";
-$lang['experience'] = "Exp&eacute;riences Professionnelles";
+$lang['experience'] = "Expériences Professionnelles";
 $lang['duties'] = "T&acirc;ches";
 $lang['education'] = "Formation";
 
-$lang['download_webcv'] = "T&eacute;l&eacute;charger CV Web &mdash; PDF";
-$webFileDate = new DateTime('01/05/2017');
-$lang['webfile_size'] = 91;
+$lang['download_webcv'] = "Télécharger CV Web &mdash; PDF";
+$webFileDate = new DateTime('02/03/2017');
+$lang['webfile_size'] = 79;
 
-$lang['telecharger_comcv'] = "T&eacute;l&eacute;charger CV Communications &mdash; PDF";
+$lang['telecharger_comcv'] = "Télécharger CV Communications &mdash; PDF";
 $prFileDate = new DateTime('01/07/2017');
 $lang['prfile_size'] = 91;
 
 $lang['ce_que_je_peux_faire_titre'] = "Ce Que Je Peux Faire";
-$lang['redaction_soustitre'] = "R&eacute;daction";
-$lang['redaction_p'] = "Puisque je d&eacute;tiens un bac en journalisme, je sais bien &eacute;crire tant en ligne comme &agrave; l'&eacute;crit et j'ai employ&eacute; mes comp&eacute;tences pour publier des nouvelles et pour mettre les petites entreprises en vedette dans l'espace de marketing.";
-$lang['multimedia_soustitre'] = "Multim&eacute;dia";
-$lang['multimedia_p'] = "Dans mes anciens jours, je travaillais en tant qu'animateur &agrave; la t&eacute;l&eacute; et monteur de vid&eacute;os. Je realisais mes propres reportages du d&eacute;but jusqu'&agrave; la fin en produisant mes propres effets sp&eacute;ciaux.";
-$lang['courriel_soustitre'] = "Conc&eacute;ption de Courriel";
-$lang['courriel_p'] = "En comprenant que la conc&eacute;ption de courriel n'est pas pareille que celle du Web, je suis comp&eacute;tent avec le code &ldquo;obsol&egrave;te&rdquo; de HTML et CSS. De plus, j'ai une bonne id&eacute;e de ce qu'Outlook accepte et n'accepte pas.";
+$lang['redaction_soustitre'] = "Rédaction";
+$lang['redaction_p'] = "Puisque je détiens un bac en journalisme, je sais bien écrire tant en ligne comme à l'écrit et j'ai employé mes compétences pour publier des nouvelles et pour mettre les petites entreprises en vedette dans l'espace de marketing.";
+$lang['multimedia_soustitre'] = "Multimédia";
+$lang['multimedia_p'] = "Dans mes anciens jours, je travaillais en tant qu'animateur à la télé et monteur de vidéos. Je realisais mes propres reportages du début jusqu'à la fin en produisant mes propres effets spéciaux.";
+$lang['courriel_soustitre'] = "Concéption de Courriel";
+$lang['courriel_p'] = "En comprenant que la concéption de courriel n'est pas pareille que celle du Web, je suis compétent avec le code &ldquo;obsolète&rdquo; de HTML et CSS. De plus, j'ai une bonne idée de ce qu'Outlook accepte et n'accepte pas.";
 $lang['codepropre_soustitre'] = "Code Propre";
-$lang['codepropre_p'] = "Je suis s&eacute;rieux en &eacute;crivant seulement du code HTML5, CSS3 et PHP7 le plus propre et &agrave; jour. Mon code est correctement dent&eacute;l&eacute; et il suit les standards &eacute;tablis par le W3C.";
+$lang['codepropre_p'] = "Je suis sérieux en écrivant seulement du code HTML5, CSS3 et PHP7 le plus propre et à jour. Mon code est correctement dentélé et il suit les standards établis par le W3C.";
 $lang['adaptatif_soustitre'] = "Sites Web Adaptifs";
-$lang['adaptatif_p'] = "Je comprend non seulement l'impl&eacute;mentation des requ&ecirc;tes m&eacute;dias pour flotter et mettre &agrave; l'&eacute;chelle du contenu pour les sites Web adaptifs, je comprend aussi les meilleures pratiques pour cr&eacute;er un courriel adaptif (si support&eacute;).";
+$lang['adaptatif_p'] = "Je comprend non seulement l'implémentation des requ&ecirc;tes médias pour flotter et mettre à l'échelle du contenu pour les sites Web adaptifs, je comprend aussi les meilleures pratiques pour créer un courriel adaptif (si supporté).";
 $lang['soutien_soustitre'] = "Soutien";
-$lang['soutien_p'] = "Bien que j'offre des services Web pour mon association, j'entra&icirc;ne aussi nos employ&eacute;s pour qu'il puissent g&eacute;rer leur propre contenu et j'assiste nos membres en anglais, fran&ccedil;ais et espagnol avec leurs questions d'adh&eacute;sion.";
+$lang['soutien_p'] = "Bien que j'offre des services Web pour mon association, j'entra&icirc;ne aussi nos employés pour qu'il puissent gérer leur propre contenu et j'assiste nos membres en anglais, fran&ccedil;ais et espagnol avec leurs questions d'adhésion.";
 
 $lang['status_title'] = 'Mon status';
-$lang['status_1'] = "Je travaille actuellement pour l'Association am&eacute;ricaine de science politique";
-$lang['status_2'] = "Je travaille dans le D&eacute;partement des communications &amp; marketing avec une &eacute;quipe formidable.";
+$lang['status_1'] = "Je travaille actuellement pour l'Association américaine de science politique";
+$lang['status_2'] = "Je travaille dans le Département des communications &amp; marketing avec une équipe formidable.";
 $lang['status_3'] = 'Contactez-moi';
 
 $lang['contact_title'] = 'Contactez-Moi';
 $lang['contact_subtitle'] = "Donnez-moi un coup de fil ou envoyez-moi un courriel. J'ai h&acirc;te de vous parler.";
-$lang['contact_paragraph'] = "Vous pouvez prendre contact avec moi en utilisant un des moyens au-dessous<span class='formuleDroite'> ou en essayant le formulaire &agrave; droite</span><span class='formuleBas' style='display:none;'>au-dessus</span>.";
+$lang['contact_paragraph'] = "Vous pouvez prendre contact avec moi en utilisant un des moyens au-dessous<span class='formuleDroite'> ou en essayant le formulaire à droite</span><span class='formuleBas' style='display:none;'>au-dessus</span>.";
 
-$lang['customers'] = "Soci&eacute;t&eacute;s pour lesquelles j'ai travaill&eacute;";
+$lang['customers'] = "Sociétés pour lesquelles j'ai travaillé";
 
 $lang['formule_nom'] = "Nom";
 $lang['formule_courriel'] = "Courriel";
 $lang['formule_message'] = "Votre Message";
 $lang['formule_envoyer'] = "Envoyez-le";
 
-$lang['copyright'] = "Grant Muka&iuml;. Tous droits r&eacute;serv&eacute;s. Ic&ocirc;nes des drapeaux gracieusement fournies par <a href='http://www.icondrawer.com'>Icon Drawer</a>.";
-$lang['creditdelaphoto'] = "Cr&eacute;dit de la Photo Ent&ecirc;te";
+$lang['copyright'] = "Grant Mukaï. Tous droits réservés. Ic&ocirc;nes des drapeaux gracieusement fournies par <a href='http://www.icondrawer.com'>Icon Drawer</a>.";
+$lang['creditdelaphoto'] = "Crédit de la Photo Ent&ecirc;te";
 $lang['par'] = "par";
-$lang['license2photo'] = "Autoris&eacute;e sous le Permis Creative Commons. Aucune modification n'a &eacute;t&eacute; appliqu&eacute;e &agrave; cette photo."
+$lang['license2photo'] = "Autorisée sous le Permis Creative Commons. Aucune modification n'a été appliquée à cette photo."
 ?>
